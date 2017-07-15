4b59e03426a9d4cec3777f1ace0779048ca94bb4
ProDataMan <ProDataMan@outlook.com>
2017-07-15 14:47:26 -0700

    Updated pop and pushpop tests

--- Stack Utility/src/pdm/stack/util/Stack.java
+++ Stack Utility/src/pdm/stack/util/Stack.java
@@ -3,11 +3,28 @@ package pdm.stack.util;
 import java.util.ArrayList;
 
 public class Stack {
-	ArrayList<String> stack = new ArrayList<String>();
-
+	ArrayList<String> stack; 
+public Stack() {
+	this.stack = new ArrayList<String>();
+}
 	public boolean isEmpty() {
 		// TODO Auto-generated method stub
-		return stack.isEmpty();
+		return this.stack.isEmpty();
+	}
+
+	public boolean push(String expected) {
+		// TODO Auto-generated method stub
+		return this.stack.add(expected);
+	}
+	public String peek() {
+		// TODO Auto-generated method stub
+		return stack.get(0);
+	}
+	public String pop() {
+		// TODO Auto-generated method stub
+		String result = stack.get(0);
+		stack.remove(0);
+		return result;
 	}
 
 }
--- Stack Utility/src/pdm/stack/util/StackTest.java
+++ Stack Utility/src/pdm/stack/util/StackTest.java
@@ -2,11 +2,47 @@ package pdm.stack.util;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
+import org.junit.Before;
+
 public class StackTest {
+	Stack stack;
+	@Before
+	public void setup() {
+		stack = new Stack();	
+	}
 @Test
 public void isEmptyTest(){
-	Stack stack = new Stack();
+	
 	boolean result = stack.isEmpty();
-	assertTrue("Developer do your job!", result);
+	assertTrue("True if Empty", result);
 }
+@Test
+public void pushTest(){
+	String expected = "Item 1";
+	boolean actual = stack.push(expected);
+	assertTrue("Shoult get " + expected, actual);
+}
+@Test
+public void peekTest(){
+	String expected = "Item 1";
+	stack.push(expected);
+	String actual = stack.peek();
+	assertEquals(expected, actual);
+}
+@Test
+public void popTest(){
+	String expected = "Item 1";
+	stack.push(expected);
+	String actual = stack.pop();
+	assertEquals(expected, actual);
+}
+@Test
+public void pushPopIsEmtpyTest(){
+	String expected = "Item 1";
+	stack.push(expected);
+	stack.pop();
+	boolean result = stack.isEmpty();
+	assertTrue("Shoult get false", result);
+}
+
 }
